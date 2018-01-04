119d23c83f78b4b7925ce9a667f03bc831b67cd1
Wan <jrw8217@gmail.com>
2018-01-03 20:32:11 -0800

    Add check_melody.py

---
+++ check_melody.py
@@ -0,0 +1,63 @@
+import json
+import os
+import sys
+
+
+
+
+def check_is_drum_of_Grand_Piano(midi_folder):
+    invalid_files_counter = 0
+
+    song_count = 0
+    melody_track_count = 0
+    no_melody_track = []
+
+    for path, subdirs, files in os.walk(midi_folder):
+
+
+
+        for name in files:
+
+            _path = path.replace('\\', '/') + '/'
+            _name = name.replace('\\', '/')
+
+
+            try:
+                if _name == 'instruments.json':
+                    song_count += 1
+                    print _path
+                    with open(_path + _name) as inst:
+                        dict = json.load(inst)
+
+
+                    melody_count = 0
+                    for key, dic in zip(dict.keys(), dict.values()):
+                        if dic['name'].lower() == 'melody':
+                            print 'key: ', key
+                            print dic
+                            melody_count += 1
+
+                    print 'melody count = ', melody_count
+
+                    if melody_count > 0:
+                        melody_track_count += 1
+                    else:
+                        no_melody_track.append(_path)
+
+
+
+            except:
+                exception_str = 'Unexpected error in ' + name,  sys.exc_info()[0]
+                print(exception_str)
+                invalid_files_counter += 1
+
+
+    print '-----------------------------------------------'
+    print 'song count: ', song_count
+    print 'melody track count: ', melody_track_count
+    print 'no melody count: ', no_melody_track
+
+
+if __name__ == '__main__':
+    path = '/home/wan/Documents/projects/data_processed/test_lmd_processed'
+    check_is_drum_of_Grand_Piano(path)
