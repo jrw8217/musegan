c3b76edc03f89b86ca619fdfa0aaa00df0eaebbb
Wan <jrw8217@gmail.com>
2018-01-06 16:00:57 +0900

    Modified check_melody

--- check_melody.py
+++ check_melody.py
@@ -27,16 +27,16 @@ def check_is_drum_of_Grand_Piano(midi_folder):
                     song_count += 1
                     print _path
                     with open(_path + _name) as inst:
-                        dict = json.load(inst)
+                        dictionary = json.load(inst)
 
 
                     melody_count = 0
-                    for key, dic in zip(dict.keys(), dict.values()):
+                    for key, dic in zip(dictionary.keys(), dictionary.values()):
                         if dic['name'].lower() == 'melody':
                             print 'key: ', key
                             print dic
                             melody_count += 1
-
+			    
                     print 'melody count = ', melody_count
 
                     if melody_count > 0:
@@ -44,7 +44,7 @@ def check_is_drum_of_Grand_Piano(midi_folder):
                     else:
                         no_melody_track.append(_path)
 
-
+		    print 'melody track count = ', melody_track_count
 
             except:
                 exception_str = 'Unexpected error in ' + name,  sys.exc_info()[0]
@@ -55,9 +55,9 @@ def check_is_drum_of_Grand_Piano(midi_folder):
     print '-----------------------------------------------'
     print 'song count: ', song_count
     print 'melody track count: ', melody_track_count
-    print 'no melody count: ', no_melody_track
+    print 'no melody midi: ', no_melody_track[:10]
 
 
 if __name__ == '__main__':
-    path = '/home/wan/Documents/projects/data_processed/test_lmd_processed'
+    path = '/data/midi/lmd_matched_processed'
     check_is_drum_of_Grand_Piano(path)
