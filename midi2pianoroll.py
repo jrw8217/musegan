4327fca66cc028765c3c288241fa2d7ef0b072d8
Wan <jrw8217@gmail.com>
2018-04-16 01:31:43 -0700

    Memory test

--- midi2pianoroll.py
+++ midi2pianoroll.py
@@ -809,14 +809,16 @@ def midi_to_pianorolls(midi_path, beat_resolution=4):
     print('-------------------------------------------------------------------------')
     print(midi_path)
     try:
-        # score = music21.converter.parse(midi_path)
-        # key = score.analyze('Krumhansl')
-        # key_name = key.tonic.name + key.type
-        # print('Krumhansl key:', key_name)
+        score = music21.converter.parse(midi_path)
+        key = score.analyze('Krumhansl')
+        key_name = key.tonic.name + key.type
+        print('Krumhansl key:', key_name)
+        del score
 
         pm = pretty_midi.PrettyMIDI(midi_path)
         # result = get_piano_rolls_with_Krumhansl_key(pm, key_name, beat_resolution)
         result = get_piano_rolls_with_estimated_key(pm, beat_resolution)
+        del key
 
     except Exception as error:
         print(error)
