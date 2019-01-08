41c65d8f30feae5366cd9d3887b1ac4707394f36
Wan <jrw8217@gmail.com>
2019-01-07 22:41:29 -0800

    Minor fix

--- lpd_cleansed_genre.py
+++ lpd_cleansed_genre.py
@@ -8,9 +8,11 @@ def save_dict_to_json(data, filepath):
     with open(filepath, 'w') as outfile:
         json.dump(data, outfile)
 
+with open('lmd_genre.pkl', 'r') as f:
+    genre_dict = pickle.load(f)
 
 
-datapath = '/tmp/data1/lakh/lpd_cleansed'
+datapath = '/data1/lakh/lpd_cleansed'
 
 for path, subdirs, files in os.walk(datapath):
     if subdirs:
@@ -19,14 +21,11 @@ for path, subdirs, files in os.walk(datapath):
     print(files)
     filepath = os.path.join(path, files[0])
 
-    with open('lmd_genre.pkl', 'r') as f:
-        genre_dict = pickle.load(f)
-
     track_name = filepath.split('/')[-2]
     print(track_name)
     if track_name not in genre_dict.keys():
         print("not have genre info")
         continue
 
-    save_dict_to_json({files[0][:-4] : genre_dict[track_name]}, os.path.join(path, 'genre.json'))
+    save_dict_to_json({'genre' : genre_dict[track_name]}, os.path.join(path, 'info.json'))
 
